80bdd7332388602a3b94f5cf28d7f23b13109f20
reasonjun <lsj110507@gmail.com>
2025-04-13 11:41:27 +0900

    feat: add Input component

---
+++ src/components/atoms/Input/index.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Input } from ".";
+
+const meta = {
+  title: "Atoms/Input",
+  component: Input,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Text: Story = {
+  args: { type: "text", label: "Text" },
+};
+
+export const Number: Story = { args: { type: "number", label: "Number" } };
