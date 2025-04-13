a8469bf35806d795fcf7688b22a962587f1908ec
reasonjun <lsj110507@gmail.com>
2025-04-13 13:55:33 +0900

    feat: add Checkbox component

---
+++ src/components/atoms/Checkbox/index.tsx
@@ -0,0 +1,49 @@
+import { Ref } from "react";
+import clsx from "clsx";
+import styles from "./styles.module.scss";
+
+export interface InputProps {
+  label?: string;
+  disabled?: boolean;
+  indeterminate?: boolean;
+  placeholder?: string;
+  name?: string;
+  id?: string;
+  checked?: boolean;
+  value?: string | number;
+  onChange?: () => void;
+  ref?: Ref<HTMLInputElement>;
+}
+
+export const Checkbox = ({
+  label,
+  disabled,
+  indeterminate,
+  name,
+  id,
+  checked,
+  value,
+  onChange,
+  ref,
+  ...props
+}: InputProps) => {
+  return (
+    <input
+      type="checkbox"
+      className={clsx(styles.module)}
+      name={name}
+      id={id}
+      checked={checked}
+      value={value}
+      disabled={disabled}
+      onChange={onChange}
+      aria-label={label}
+      {...props}
+      ref={(el) => {
+        if (el && indeterminate !== undefined) {
+          el.indeterminate = indeterminate;
+        }
+      }}
+    />
+  );
+};
