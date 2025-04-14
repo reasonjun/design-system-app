d6fd3257f94b33f919923224d97f77035844ec9a
reasonjun <lsj110507@gmail.com>
2025-04-14 09:32:43 +0900

    feat: add ErrorMessage component

---
+++ src/components/atoms/ErrorMessage/index.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ErrorMessage } from "./";
+
+const meta = {
+  title: "Atoms/ErrorMessage",
+  component: ErrorMessage,
+  tags: ["autodocs"],
+} satisfies Meta<typeof ErrorMessage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: { children: "에러" },
+};
