5217320de15a40ec2ec2f704428211d7c2a9c41a
reasonjun <lsj110507@gmail.com>
2025-04-14 09:32:56 +0900

    feat: add DescriptionMessage component

---
+++ src/components/atoms/DescriptionMessage/index.tsx
@@ -0,0 +1,11 @@
+import { ComponentPropsWithoutRef, Ref } from "react";
+import clsx from "clsx";
+import styles from "./styles.module.scss";
+
+interface Props extends ComponentPropsWithoutRef<"p"> {
+  ref?: Ref<HTMLParagraphElement>;
+}
+
+export const DescriptionMessage = ({ className, ref, ...props }: Props) => {
+  return <p className={clsx(className, styles.module)} {...props} ref={ref} />;
+};
