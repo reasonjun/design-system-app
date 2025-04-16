dfb3dfd5553b94b308f39fa3d990768b5ee8f952
reasonjun <reasonjun@daum.net>
2025-04-16 10:53:20 +0900

    chore: modularize config and add Storybook integration

--- eslint.config.js
+++ eslint.config.js
@@ -3,16 +3,17 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import storybook from "eslint-plugin-storybook";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config(
   { ignores: ["dist"] },
+
+  // JavaScript
+  js.configs.recommended,
+
+  // TypeScript
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      eslintConfigPrettier,
-    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -30,4 +31,12 @@ export default tseslint.config(
       ],
     },
   },
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+
+  // Storybook
+  storybook.configs["flat/recommended"],
+
+  // Prettier
+  eslintConfigPrettier,
 );
